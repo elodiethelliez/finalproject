384981d581142846d3f9280d2c746cf00f303ef2
elodiethelliez <elodie.thelliez@colorado.edu>
2022-12-01 11:26:17 -0700

    styling added and page flip working!

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,14 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import HTMLFlipBook from 'react-pageflip';
 
 function MyBook(props) {
     return (
-        <HTMLFlipBook width={300} height={500}>
-            <div className="demoPage">Page 1</div>
-            <div className="demoPage">Page 2</div>
-            <div className="demoPage">Page 3</div>
-            <div className="demoPage">Page 4</div>
+        <HTMLFlipBook width={500} height={648} className="zine">
+            <div className="page"><img src="blankblack.png" alt=""></img>Page 1</div>
+            <div className="page">Page 2</div>
+            <div className="page">Page 3</div>
+            <div className="page">Page 4</div>
         </HTMLFlipBook>
     );
 }
