634b0ab10f35515199d0a445f9f1b50b3c423981
anlo4139 <94018154+annalow@users.noreply.github.com>
2022-12-04 13:44:04 -0700

    first image

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import HTMLFlipBook from 'react-pageflip';
 import test from './media/test.png'
+import image1 from './media/patterns-01.jpg';
 
 function MyBook(props) {
     return (
@@ -15,6 +16,9 @@ function MyBook(props) {
                 <img src={test} alt="test" style={{ width: '500px', }}/>
             </div>
             <div className="page">Page 3</div>
+            <div className="page">
+                <img src={image1} alt="test" style={{ width: '500px', }}/>
+            </div>
         </HTMLFlipBook>
     );
 }
