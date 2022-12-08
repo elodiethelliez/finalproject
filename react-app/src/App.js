8e1955c34cc28da1a69bcb5d7fc944ed316b0a44
elodiethelliez <elodie.thelliez@colorado.edu>
2022-12-07 18:20:12 -0700

    added 2 pages

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,32 +1,28 @@
 // import logo from './logo.svg';
 import './App.css';
 import HTMLFlipBook from 'react-pageflip';
+import circles from './media/circles.png';
 import magnetic from './media/magnetic.png';
-import image1 from './media/patterns-01.jpg';
+import HY from './media/HY.jpg';
 
 function MyBook(props) {
     return (
         <HTMLFlipBook width={500} height={648} className="zine">
             <div className="page">first page </div>
             <div className="page">
+                <img src={circles} alt="circles page" style={{ width: '500px',}}/>
+            </div>
+            <div className="page">
+                <img src={HY} alt="HY pattern" style={{ width: '500px', }}/>
+            </div>
+            <div className="page">a black one should go here </div>
+            <div className="page">
                 <img src={magnetic} alt="magnetic page" style={{ width: '500px',}}/>
             </div>
-            <div className="page">Page 2
-                {/* <div class="ring">
-                    <ul>
-                        <li id="lost">lost</li>
-                        <li id="in">in</li>
-                        <li id="space">space</li>
-                    </ul>
-                </div> */}
-            </div>
-            <div className="page">
-                <img src={test} alt="test" style={{ width: '500px', }}/>
-            </div>
+
+            <div className="page"> page </div>
             <div className="page">Page 3</div>
-            <div className="page">
-                <img src={image1} alt="test" style={{ width: '500px', }}/>
-            </div>
+            
             <div className="page">Page 3</div>
             <div className="page">Page 3</div>
         </HTMLFlipBook>
