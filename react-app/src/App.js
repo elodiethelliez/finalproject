2847377ac88a1045b98c4f86c0c14d5ccc1c797a
elodiethelliez <elodie.thelliez@colorado.edu>
2022-12-07 21:32:44 -0700

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import HTMLFlipBook from 'react-pageflip';
 import circles from './media/circles.png';
 import magnetic from './media/magnetic.png';
+import matrix1 from './media/matrix1.png';
+import matrix2 from './media/matrix2.png';
 import HY from './media/HY.jpg';
 
 function MyBook(props) {
@@ -19,6 +21,12 @@ function MyBook(props) {
             </div>
             <div className="page">a black one should go here </div>
             <div className="page">
+                <img src={matrix1} alt="matrix" style={{ width: '500px',}}/>
+            </div>
+            <div className="page">
+                <img src={matrix2} alt="matrix" style={{ width: '500px',}}/>
+            </div>
+            <div className="page">
                 <img src={magnetic} alt="magnetic page" style={{ width: '500px',}}/>
             </div>
 
@@ -26,7 +34,7 @@ function MyBook(props) {
             <div className="page">Page 3</div>
             
             <div className="page">Page 3</div>
-            <div className="page">back cover</div>
+            <div className="hiddenpage"></div>
         </HTMLFlipBook>
     );
 }
