613019895b1fe09270f13a74d55c7d41f5acbdbe
elodiethelliez <elodie.thelliez@colorado.edu>
2022-12-07 17:46:15 -0700

    adding page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
 import HTMLFlipBook from 'react-pageflip';
-import test from './media/test.png'
+import magnetic from './media/magnetic.png';
 import image1 from './media/patterns-01.jpg';
 
 function MyBook(props) {
     return (
         <HTMLFlipBook width={500} height={648} className="zine">
-            <div className="page">Page 1 </div>
+            <div className="page">first page </div>
+            <div className="page">
+                <img src={magnetic} alt="magnetic page" style={{ width: '500px',}}/>
+            </div>
             <div className="page">Page 2
-                <div class="ring">
+                {/* <div class="ring">
                     <ul>
                         <li id="lost">lost</li>
                         <li id="in">in</li>
                         <li id="space">space</li>
                     </ul>
-                </div>
+                </div> */}
             </div>
             <div className="page">
                 <img src={test} alt="test" style={{ width: '500px', }}/>
